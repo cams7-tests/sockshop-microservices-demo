a81b13781aca4dca294411af8affc59aa14b55bf
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-08-08 15:56:16 -0300

    Update project

--- deploy/docker-compose/grafana/import.sh
+++ deploy/docker-compose/grafana/import.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env sh
 
 sleep 3
-# Import data sources
-for file in *-datasource.json; do
-  if [ -e "$file" ]; then
-    echo "importing $file" &&
-    curl --silent --fail --show-error \
-      --request POST http://admin:foobar@grafana:3000/api/datasources \
-      --header "Content-Type: application/json" \
-      --header "Accept: application/json" \
-      --data-binary "@$file";
-    echo "";
-  fi
+# Import dashboards
+for DASHBOARD in sock-shop-performance-dashboard; do
+  echo "importing $DASHBOARD" &&
+  curl --request POST http://admin:foobar@grafana:3000/api/dashboards/import \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/json" \
+    --data-binary "@${DASHBOARD}.json";
 done;
 
-# Import dashboards
-for file in *-dashboard.json; do
-  if [ -e "$file" ]; then
-    echo "importing $file" &&
-    curl --request POST http://admin:foobar@grafana:3000/api/dashboards/import \
-      --header "Content-Type: application/json" \
-      --header "Accept: application/json" \
-      --data-binary "@$file";
-    echo "";
-  fi
-done;
+for DASHBOARD in prometheus-dashboard; do
+echo "Importing ${DASHBOARD}.json..." &&
+curl -s -k -u "admin:foobar" -XPOST -H "Accept: application/json" -H "Content-Type: application/json" \
+-d @- "http://grafana:3000/api/dashboards/import" <<CURL_DATA
+{"dashboard":$(cat /opt/grafana-import-dashboards/${DASHBOARD}.json),"overwrite":false, "inputs":[{"name":"DS_PROMETHEUS","type":"datasource","pluginId":"prometheus","value":"prometheus"}]}
+CURL_DATA
+done
+
+for DASHBOARD in 1860; do
+REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[-1].revision")"
+curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions/${REVISION}/download > /tmp/dashboard.json
+echo "Importing $(cat /tmp/dashboard.json | jq -r '.title') (revision ${REVISION}, id ${DASHBOARD})..." &&
+curl -s -k -o /dev/null -u "admin:foobar" -XPOST \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-d @- "http://grafana:3000/api/dashboards/import" <<CURL_DATA
+{"dashboard":$(cat /tmp/dashboard.json),"overwrite":false, "inputs":[{"name":"DS_PROMETHEUS","type":"datasource","pluginId":"prometheus","value":"prometheus"}]}
+CURL_DATA
+done
